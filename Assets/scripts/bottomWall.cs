8d9172add3defa7d4f856a588e0095815b74ddb7
Vulpes19 <baioumyaymann@gmail.com>
2023-04-07 21:40:44 +0000

    the paddle respawns now

--- Assets/scripts/bottomWall.cs
+++ Assets/scripts/bottomWall.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class bottomWall : MonoBehaviour
 {
     //public Vector2 ballStartPosition;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("paddle");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //ballStartPosition = new Vector2(Screen.width / 2, Screen.height / 2);
@@ -14,7 +20,8 @@ public class bottomWall : MonoBehaviour
             LivesManager.instance.loseLife();
             ball.velocity = Vector2.zero;
             //ball.transform.position = Vector2.zero;
-            ball.transform.position = new Vector2(0.04f, -1f);
+            ball.transform.position = new Vector2(0f, -1f);
+            player.transform.position = new Vector2(0f, -4.79f);
            // ball.velocity = new Vector2(Random.Range(-1f, 1f), 1).normalized * 4f;
             //ball.velocity = new Vector2(0.04f, -3.56f);
         }
