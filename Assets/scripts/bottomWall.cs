80c8f5a398ef45f1bd36263bb2cc65319594f747
Vulpes19 <baioumyaymann@gmail.com>
2023-04-07 21:24:25 +0000

    the ball respawns when it's out of bounds, but not in the correct position

--- Assets/scripts/bottomWall.cs
+++ Assets/scripts/bottomWall.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class bottomWall : MonoBehaviour
 {
-
+    //public Vector2 ballStartPosition;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //ballStartPosition = new Vector2(Screen.width / 2, Screen.height / 2);
         if ( collision.gameObject.CompareTag("Ball") )
         {
+            Rigidbody2D ball = collision.gameObject.GetComponent<Rigidbody2D>();
             LivesManager.instance.loseLife();
+            ball.velocity = Vector2.zero;
+            ball.transform.position = Vector2.zero;
+            ball.position = new Vector2(0.04f, -3.56f);
+           // ball.velocity = new Vector2(Random.Range(-1f, 1f), 1).normalized * 4f;
+            //ball.velocity = new Vector2(0.04f, -3.56f);
         }
     }
 }
