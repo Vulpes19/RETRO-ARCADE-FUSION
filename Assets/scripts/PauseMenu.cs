e554788a2c806a2f585fed4a36747af27e94d616
Vulpes19 <baioumyaymann@gmail.com>
2023-04-26 18:02:04 +0100

    working on pause menu buttons

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject menu;
+    public GameObject exitButton;
+    public GameObject retryButton;
+    public GameObject settingsButton;
     public bool isPaused;
     
     void Start()
@@ -27,6 +32,9 @@ public class PauseMenu : MonoBehaviour
     {
         isPaused = false;
         menu.SetActive(false);
+        exitButton.SetActive(false);
+        retryButton.SetActive(false);
+        settingsButton.SetActive(false);
         Time.timeScale = 1.0f;
     }
 
@@ -34,6 +42,24 @@ public class PauseMenu : MonoBehaviour
     {
         isPaused = true;
         menu.SetActive(true);
+        exitButton.SetActive(true);
+        retryButton.SetActive(true);
+        settingsButton.SetActive(true);
+        Button b1 = exitButton.GetComponent<Button>();
+        Button b2 = retryButton.GetComponent<Button>();
+        Button b3 = settingsButton.GetComponent<Button>();
+        b1.onClick.AddListener(quitGame);
+        b2.onClick.AddListener(retryGame);
         Time.timeScale = 0.0f;
+    }
+
+    public void retryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
     }
 }
