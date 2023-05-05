2ee56c3c5c633be96618005fbab90dac8636dfe1
Vulpes19 <baioumyaymann@gmail.com>
2023-05-05 15:55:15 +0100

    the particles spawn but for some reason they keep following the ball

--- Assets/scripts/ballMovement.cs
+++ Assets/scripts/ballMovement.cs
@@ -12,7 +12,7 @@ public class BallMovement : MonoBehaviour
     private Rigidbody2D paddle;
     private float maxRotation = 50f;
     private Vector3 paddleScale;
-    [SerializeField] public GameObject particles;
+    [SerializeField] public ParticleSystem particles;
     Vector2 direction;
     void Start()
     {
@@ -35,61 +35,72 @@ public class BallMovement : MonoBehaviour
         ball.velocity = direction * speed;
     }
 
+    private void handleScore( Vector3 hitPosition, string brick = "one" )
+    {
+        if (hitPosition.y >= 2 && hitPosition.y < 3)
+            brick = "three";
+        else if (hitPosition.y >= 3 && hitPosition.y < 4)
+            brick = "five";
+        else if (hitPosition.y >= 4)
+        {
+            brick = "seven";
+            paddleObject = GameObject.FindGameObjectWithTag("paddle");
+            if (paddleObject)
+            {
+                paddle = paddleObject.GetComponent<Rigidbody2D>();
+                if (paddle.transform.localScale != paddleScale)
+                    paddle.transform.localScale = new Vector3(paddle.transform.localScale.x / 2f, paddle.transform.localScale.y, paddle.transform.localScale.z);
+            }
+            speed = 6f;
+        }
+        ScoreManager.instance.updateScore(brick);
+    }
+    private void destroyBricks( Tilemap tilemap, Collision2D collision )
+    {
+        Vector3 hitPosition = Vector3.zero;
+        Vector3 particlesPos = Vector3.zero;
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            hitPosition.x = contact.point.x - 0.01f * contact.normal.x;
+            hitPosition.y = contact.point.y - 0.01f * contact.normal.y;
+            particlesPos = contact.point;
+            Vector3Int brickPosition = tilemap.WorldToCell(hitPosition);
+            Vector3 brickWorldPosition = tilemap.CellToWorld(brickPosition);
+            if (tilemap.HasTile(brickPosition))
+            {
+                hitPosition.y += 1;
+                Scene activeScene = SceneManager.GetActiveScene();
+                if (activeScene.name != "MainMenu")
+                {
+                    handleScore(hitPosition);
+                }
+                tilemap.SetTile(brickPosition, null);
+                if (activeScene.name != "MainMenu")
+                {
+                    //particles.transform.localScale = new Vector3(particles.transform.localScale.x, particles.transform.localScale.y, 1);
+                    //Instantiate(particles, brickWorldPosition, Quaternion.identity);
+                    particles.transform.position = brickWorldPosition;
+                    particles.Play();
+                }
+                //particles.Stop();
+                float dotProduct = Vector2.Dot(contact.normal, Vector2.right);
+                if (dotProduct > 0f)
+                    direction.x = Mathf.Abs(direction.x);
+                else if (dotProduct < 0f)
+                    direction.x = -Mathf.Abs(direction.x);
+                else
+                    direction.y *= -1;
+            }
+
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject tilemapObject = GameObject.Find("bricks");
         Tilemap tilemap = tilemapObject.GetComponent<Tilemap>();
         if (tilemap)
         {
-            Vector3 hitPosition = Vector3.zero;
-            foreach (ContactPoint2D contact in collision.contacts)
-            {
-                hitPosition.x = contact.point.x - 0.01f * contact.normal.x;
-                hitPosition.y = contact.point.y - 0.01f * contact.normal.y;
-                Vector3Int brickPosition = tilemap.WorldToCell(hitPosition);
-                if (tilemap.HasTile(brickPosition))
-                {
-                    hitPosition.y += 1;
-                    Scene activeScene = SceneManager.GetActiveScene();
-                    if (activeScene.name != "MainMenu")
-                    {
-                        string brick = "one";
-                        if (hitPosition.y >= 2 && hitPosition.y < 3)
-                            brick = "three";
-                        else if (hitPosition.y >= 3 && hitPosition.y < 4)
-                            brick = "five";
-                        else if (hitPosition.y >= 4)
-                        {
-                            brick = "seven";
-                            paddleObject = GameObject.FindGameObjectWithTag("paddle");
-                            if (paddleObject)
-                            {
-                                paddle = paddleObject.GetComponent<Rigidbody2D>();
-                                if (paddle.transform.localScale != paddleScale)
-                                    paddle.transform.localScale = new Vector3(paddle.transform.localScale.x / 2f, paddle.transform.localScale.y, paddle.transform.localScale.z);
-                            }
-                            speed = 6f;
-                        }
-                        ScoreManager.instance.updateScore(brick);
-                    }
-                    tilemap.SetTile(brickPosition, null);
-                    if (activeScene.name != "MainMenu")
-                    {
-                        particles.transform.localScale = new Vector3(particles.transform.localScale.x, particles.transform.localScale.y, 1);
-                        hitPosition.z = 1;
-                        Instantiate(particles, hitPosition, Quaternion.identity);
-                        //particles.gameObject.SetActive(true);
-                    }
-                     float dotProduct = Vector2.Dot(contact.normal, Vector2.right);
-                    if (dotProduct > 0f)
-                        direction.x = Mathf.Abs(direction.x);
-                    else if (dotProduct < 0f)
-                        direction.x = -Mathf.Abs(direction.x);
-                    else
-                        direction.y *= -1;
-                }
-
-            }
+            destroyBricks(tilemap, collision);
         }
         if (collision.gameObject.CompareTag("paddle") || collision.gameObject.CompareTag("topWall"))
         {
