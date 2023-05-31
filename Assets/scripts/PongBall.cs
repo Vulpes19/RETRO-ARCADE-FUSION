016ad0afa67886ffb1f812ec4c1b7bef8033242e
Vulpes19 <baioumyaymann@gmail.com>
2023-05-31 16:38:44 +0100

    The score system is working now

--- Assets/scripts/PongBall.cs
+++ Assets/scripts/PongBall.cs
@@ -7,12 +7,15 @@ public class PongBall : MonoBehaviour
     private Rigidbody2D ball;
     public float maxRotation;
     public float speed;
+    private Vector3 worldCenter;
     private Vector2 direction;
+    private GameObject player;
+    private GameObject AI;
     void Start()
     {
         ball = GetComponent<Rigidbody2D>();
         Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
-        Vector3 worldCenter = Camera.main.ScreenToWorldPoint(screenCenter);
+        worldCenter = Camera.main.ScreenToWorldPoint(screenCenter);
         worldCenter.z = 0;
         ball.transform.position = worldCenter;
         float angle = Random.Range(-maxRotation, maxRotation);
@@ -25,12 +28,30 @@ public class PongBall : MonoBehaviour
         ball.velocity = direction * speed;
     }
 
+    private void RespawnBallAndPaddles()
+    {
+        ball.velocity = Vector2.zero;
+        ball.transform.position = worldCenter;
+        player = GameObject.FindGameObjectWithTag("Player");
+        AI = GameObject.FindGameObjectWithTag("AI");
+        player.transform.position = new Vector2(-9.8808f, -0.39f);
+        AI.transform.position = new Vector2(9.882f, -0.39f);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("rightWall"))
+        if ( collision.gameObject.CompareTag("rightWall"))
+        {
             PongScoreManager.instance.updateScore("player");
+            RespawnBallAndPaddles();
+        }
         if (collision.gameObject.CompareTag("leftWall"))
+        {
             PongScoreManager.instance.updateScore("AI");
+            RespawnBallAndPaddles();
+        }
+
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("AI") )
+            direction.x *= -1;
         if (collision.gameObject.CompareTag("topWall") || collision.gameObject.CompareTag("bottomWall"))
             direction.y *= -1;
     }
