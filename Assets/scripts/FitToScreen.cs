2fd0e8bc29eef099cac3555b4f6a033f6a3bc074
Vulpes19 <baioumyaymann@gmail.com>
2023-06-07 17:24:42 +0100

    for some reason the play and settings buttons in the main menu dont work anymore

--- Assets/scripts/FitToScreen.cs
+++ Assets/scripts/FitToScreen.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class FitToScreen : MonoBehaviour
 {
     private Camera mainCamera;
+    private GameObject cameraObject;
     void Start()
     {
-        mainCamera = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
-        transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, 0);
-        Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(mainCamera.rect.width, mainCamera.rect.height)) * 100;
-        Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(Vector3.zero) * 100;
-        Vector3 screenSize = topRight - bottomLeft;
-        float screenRatio = screenSize.x / screenSize.y;
-        float desiredRatio = transform.localScale.x / transform.localScale.y;
-        if ( screenRatio > desiredRatio )
+        cameraObject = GameObject.FindGameObjectWithTag("Main Camera");
+        if (cameraObject)
         {
-            float height = screenSize.y;
-            transform.localScale = new Vector3(height * desiredRatio, height);
-        }
-        else
-        {
-            float width = screenSize.x;
-            transform.localScale = new Vector3(width, width / desiredRatio);
+            mainCamera = cameraObject.GetComponent<Camera>();
+            transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, 0);
+            Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(mainCamera.rect.width, mainCamera.rect.height)) * 100;
+            Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(Vector3.zero) * 100;
+            Vector3 screenSize = topRight - bottomLeft;
+            float screenRatio = screenSize.x / screenSize.y;
+            float desiredRatio = transform.localScale.x / transform.localScale.y;
+            if (screenRatio > desiredRatio)
+            {
+                float height = screenSize.y;
+                transform.localScale = new Vector3(height * desiredRatio, height);
+            }
+            else
+            {
+                float width = screenSize.x;
+                transform.localScale = new Vector3(width, width / desiredRatio);
+            }
         }
     }
 
